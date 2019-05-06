4933ff852e9b8a24939485d42748498e98df85b4
Vyacheslav Voronenko <git@voronenko.info>
2019-05-06 15:14:40 +0200

    chore(gitignore) offline preload boxes

--- add_boxes.sh
+++ add_boxes.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# https://vagrantcloud.com/centos/boxes/7/versions/1902.01/providers/virtualbox.box
 vagrant box add centos/6
+# https://vagrantcloud.com/centos/boxes/7/versions/1902.01/providers/virtualbox.box
 vagrant box add centos/7
+# https://vagrantcloud.com/ubuntu/boxes/trusty64/versions/20190429.0.0/providers/virtualbox.box
 vagrant box add ubuntu/trusty64
 vagrant box add ubuntu/xenial64
 vagrant box add ubuntu/bionic64
+# https://vagrantcloud.com/mwrock/boxes/Windows2016/versions/0.3.0/providers/virtualbox.box
 vagrant box add mwrock/Windows2016
+# https://vagrantcloud.com/mwrock/boxes/Windows2012R2/versions/0.6.1/providers/virtualbox.box
 vagrant box add mwrock/Windows2012R2
